c0c6f49540b9fa50fbb8bc09ad498435f08bdfdf
Puma <Pumaredblue@gmail.com>
2023-08-28 00:28:09 +0300

    add project

---
+++ PublicHolidays/Model/holiday.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  PublicHolidays
+//
+//  Created by Rimma Katushina on 27/08/2023.
+//
+
+struct Holiday: Decodable{
+    let date: String
+    let localName: String
+    let countryCode: String
+    let type: String
+
+}
+
--- PublicHolidays/ViewController.swift
+++ PublicHolidays/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showHolidayButton(_ sender: Any) {
+        fetchHoliday()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController {
+    private func fetchHoliday() {
+        guard let url = URL(string: "https://date.nager.at/api/v2/publicholidays/2020/US") else {return}
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            do {
+                let holiday = try JSONDecoder().decode([Holiday].self, from: data)
+                print(holiday)
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+}
